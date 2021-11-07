ea83892ef2824d4062f0d9c26905e3ec14bc3801
akhyaradn <akhyaradn7@gmail.com>
2021-11-07 20:32:33 +0800

    initialize project

---
+++ src/main/java/com/akhyar/tokopediascraper/TokopediaScraperApplication.java
@@ -0,0 +1,15 @@
+package com.akhyar.tokopediascraper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class TokopediaScraperApplication{
+
+	public static void main(String[] args) {
+		SpringApplication.run(TokopediaScraperApplication.class, args);
+
+	}
+}
---
+++ src/test/java/com/akhyar/tokopediascraper/TokopediaScraperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akhyar.tokopediascraper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TokopediaScraperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
